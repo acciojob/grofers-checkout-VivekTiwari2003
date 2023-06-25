63d0fd98f804be17f36d8098df3ffe4dfed28475
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-06-25 15:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,33 +8,31 @@
 // };
 
 // getSumBtn.addEventListener("click", getSum);
-
 const getSumBtn = document.createElement("button");
-getSumBtn.textContent = "Get Total Price";
+getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-  // Step 1: Get all the prices from the table
-  const prices = document.querySelectorAll('.price');
+//Add your code here
+// Step 1: Get all the prices from the table
+const prices = document.querySelectorAll('.price');
+// Step 2: Calculate the total price
+let totalPrice = 0;
+prices.forEach((price) => {
+totalPrice += parseInt(price.textContent);
+});
+// Step 3: Create a new row for the total price
+const table = document.querySelector('table');
+const newRow = document.createElement('tr');
+const newCell = document.createElement('td');
+newCell.setAttribute('id', 'ans');
+newCell.textContent = totalPrice
+newRow.appendChild(newCell);
+table.appendChild(newRow);
 
-  // Step 2: Calculate the total price
-  let totalPrice = 0;
-  prices.forEach((price) => {
-    totalPrice += parseFloat(price.textContent);
-  });
-
-  // Step 3: Create a new row for the total price
-  const table = document.querySelector('table');
-  const newRow = document.createElement('tr');
-  const newCell = document.createElement('td');
-  newCell.textContent = 'Total Price: ' + totalPrice.toFixed(2); // Assuming prices are in decimal format
-  newCell.setAttribute('colspan', '2'); // Adjust the colspan according to the number of columns in your table
-  newRow.appendChild(newCell);
-  table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
 
 
 
